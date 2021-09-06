c8c2136f483292fdb4f17c4dbab060391a48d72e
liaotangyuan <2735917073@qq.com>
2021-09-06 23:04:51 +0800

    文章管理中文章列表剩删除功能前提交

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,115 @@
+$(function () {
+    // 实例化layui对象
+    var layer = layui.layer
+    var form = layui.form
+
+    initArtCateList();
+
+    // 获取文章分类的列表
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章分类信息失败！')
+                }
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+    // 为添加分类按钮绑定点击事件
+    var indexAdd = null;
+    $('#btnAddCate').on('click', function () {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        });
+
+    })
+
+    // 通过事件代理为弹出添加分类的form表单绑定submit事件
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('添加失败:' + res.message)
+                }
+                layer.msg(res.message)
+                initArtCateList()
+                layer.close(indexAdd)
+            }
+        })
+    })
+
+    // 通过事件代理为编辑按钮绑定点击事件
+    var indexEdit = null;
+    $('tbody').on('click', '#btn-edit', function () {
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类信息',
+            content: $('#dialog-edit').html()
+        });
+
+        // 1.获取到被点击的该条数据的data-Id值
+        var id = $(this).attr('data-id')
+        // 2.发起get请求根据数据的id获取到该条数据
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates/'+id,
+            success: function(res) {
+                // 将获取到的数据渲染到弹出层中
+                form.val('form-edit', res.data)
+            }
+        })
+
+    })
+
+    // 通过事件代理为弹出编辑分类的form表单绑定submit事件
+    $('body').on('submit', '#form-edit', function(e) {
+        e.preventDefault();
+        console.log('ok');
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function(res) {
+                if(res.status !== 0) {
+                    return layer.msg('更新分类数据失败！')
+                }
+                layer.msg('更新分类数据成功！')
+                layer.close(indexEdit);initArtCateList();
+            }
+        })
+    })
+    // 通过事件代理为删除按钮绑定点击事件
+    $('tbody').on('click', '#btn-del', function(e) {
+        // 先获取该条数据的id值
+        var id = $(this).prev('#btn-edit').attr('data-id')
+        // 弹出询问框
+        layer.confirm('确认删除?', {icon: 3, title:'提示'}, function(index){
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/'+id,
+                success: function(res) {
+                    if(res.status !== 0) {
+                        return layer.msg(res.message)
+                    }
+                    layer.msg('删除文章分类成功！')
+                    layer.close(index);
+                    initArtCateList();
+                }
+            })
+            
+          });
+        
+    })
+})
---
+++ assets/js/article/art_list.js
@@ -0,0 +1,115 @@
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage;    // 分页效果
+
+    // 定义美化时间的过滤器
+    template.defaults.imports.dataFormat = function (date) {
+        const dt = new Date(date)
+        let y = dt.getFullYear()
+        y = y < 10 ? '0' + y : y
+        let m = dt.getMonth() + 1
+        m = m < 10 ? '0' + m : m
+        let d = dt.getDate()
+        d = d < 10 ? '0' + d : d
+        let hh = dt.getHours()
+        hh = hh < 10 ? '0' + hh : hh
+        let mm = dt.getMinutes()
+        mm = mm < 10 ? '0' + mm : mm
+        let ss = dt.getSeconds()
+        ss = ss < 10 ? '0' + ss : ss
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+    // 定义一个查询的参数对象，将来请求数据的时候，需要按照它的要求请求服务器数据
+    var q = {
+        pagenum: '1',  // 页码值，默认第一页数据
+        pagesize: 2,  // 每页显示几条数据，默认2条
+        cate_id: '', // 文章分类的 Id，默认查看所有
+        state: ''  // 文章的发布状态
+    }
+
+    initTable();
+    initCate();
+
+    // 发起ajax数据请求渲染页面数据
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表数据失败！')
+                }
+                console.log(res.data);
+                // 使用模板引擎渲染页面数据
+                let htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                // 渲染完表格数据后重新渲染分页的结构
+                renderPage(res.total)
+
+            }
+        })
+    }
+
+    // 初始化文章分类信息
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章分类信息失败!')
+                }
+                // 调用模板引擎将分类信息渲染到页面中
+                let htmlStr = template('tpl-case', res)
+                $('[name=cate_id]').html(htmlStr)
+                // 通知layui重新渲染表单区域UI数据
+                form.render();
+            }
+        })
+    }
+
+    // 为筛选表单区域的筛选按钮绑定submit事件
+    $('#form-seach').on('submit', '.layui-btn', function (e) {
+        e.preventDefault();
+        // 1.获取表单项中选择的值
+        let cate_id = $('[name=cate_id]').val()
+        let state = $('[name=state]').val()
+        // 2.为查询的参数对象q的对应属性赋值
+        q.cate_id = cate_id
+        q.state = state
+        // 3.根据最新的筛选条件重新渲染表格数据
+        initTable();
+    })
+
+    // 封装一个渲染分页的函数
+    function renderPage(total) {
+        // 调用laypage.render()方法来渲染分页的结构
+        laypage.render({
+            elem: 'pageBox', // 分页容器的id(不加#)
+            count: total, // 总数据条数
+            limit: q.pagesize,  // 每页显示几条数据
+            curr: q.pagenum, // 设置默认被选中的页数
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'], // 数据的项数分别代表分页区域渲染的结构
+            limits: [2, 5, 10, 20],  // 每页可选择展示条数的规格
+            
+            // 1.分页发生切换时会触发jump回调函数
+            // 2.只要调用了laypage.render()方法就会触发jump回调，这种方法触发的jump回调其内部形参first的值是true
+            jump: function (obj, first) {
+                // 把最新的页码值赋值给参数对象q
+                q.pagenum = obj.curr
+
+                //  把最新的条目数，赋值给参数对象q
+                q.pagenum = obj.limit
+
+                // 如果是第二种方式触发的jump则不调用initTable()函数，否则会成死循环
+                if (!first) {
+                    // 使用新的页码值重新渲染页面数据
+                    initTable();
+                }
+            }
+        });
+    }
+})
